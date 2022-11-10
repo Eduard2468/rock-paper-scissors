8fb93e450fbbe320570bcd0cf61a0d8849d8d816
eduard <eduardleon2468@gmail.com>
2022-11-10 14:22:26 -0800

    rps is now played with buttons vs computer

--- rps.js
+++ rps.js
@@ -13,38 +13,95 @@ check which value is greater
 */
 
 
-//asks whats you choose
-let your_choice_string = prompt("pick now!", "rock");
-
-
-
 //computer picks random choice
+/*    if (your_choice_string === "rock" && computuer_choice_pick === "rock") {
+        alert("draw");
+    } else if (your_choice_string === "paper" && computuer_choice_pick === "paper") {
+        alert("draw");
+    } else if (your_choice_string === "scissors" && computuer_choice_pick === "scissors") {
+        alert("draw");
+    }  else if (your_choice_string === "rock" && computuer_choice_pick === "paper") {
+        alert("you lose");
+    } else if (your_choice_string === "rock" && computuer_choice_pick === "scissors") {
+        alert("you win");
+    } else if (your_choice_string === "paper" && computuer_choice_pick === "rock") {
+        alert("you win");
+    } else if (your_choice_string === "paper" && computuer_choice_pick === "scissors") {
+        alert("you lose");
+    } else if (your_choice_string === "scissors" && computuer_choice_pick === "rock") {
+        alert("you lose");
+    } else if (your_choice_string === "scissors" && computuer_choice_pick === "paper") {
+        alert("you lose");
+    }
+
+
 let choice = ["rock", "paper", "scissors"];
 let computuer_choice_length = choice.length;
 let randomNumber = Math.random();
 randomNumber = randomNumber * computuer_choice_length;
 randomNumber = Math.floor(randomNumber);
 let computuer_choice_pick = choice[randomNumber];
+*/
 
-alert("NPC picks " + computuer_choice_pick);
+let your_choice_string = '';
+function rock() {
+    your_choice_string = "rock";
+    console.log(your_choice_string);
+
+    let choice = ["rock", "paper", "scissors"];
+    let computuer_choice_length = choice.length;
+    let randomNumber = Math.random();
+    randomNumber = randomNumber * computuer_choice_length;
+    randomNumber = Math.floor(randomNumber);
+    let computuer_choice_pick = choice[randomNumber];
+
+    if (computuer_choice_pick === "rock") {
+        alert("draw");
+    } else if ( computuer_choice_pick === "paper") {
+        alert("you lose");
+    } else if (computuer_choice_pick === "scissors") {
+        alert("you win");
+    }
+
+}
+function paper() {
+
+    your_choice_string = "paper";
+    console.log(your_choice_string);
+
+    let choice = ["rock", "paper", "scissors"];
+    let computuer_choice_length = choice.length;
+    let randomNumber = Math.random();
+    randomNumber = randomNumber * computuer_choice_length;
+    randomNumber = Math.floor(randomNumber);
+    let computuer_choice_pick = choice[randomNumber];
+
+    if (your_choice_string === "paper" && computuer_choice_pick === "paper") {
+        alert("draw");
+    } else if (your_choice_string === "paper" && computuer_choice_pick === "rock") {
+        alert("you win");
+    } else if (your_choice_string === "paper" && computuer_choice_pick === "scissors") {
+        alert("you lose");
+    }
+}
+function scissors() {
+    your_choice_string = "scissors";
+    console.log(your_choice_string);
+
+    let choice = ["rock", "paper", "scissors"];
+    let computuer_choice_length = choice.length;
+    let randomNumber = Math.random();
+    randomNumber = randomNumber * computuer_choice_length;
+    randomNumber = Math.floor(randomNumber);
+    let computuer_choice_pick = choice[randomNumber];
+
+    if (computuer_choice_pick === "scissors") {
+        alert("draw");
+    }  else if (computuer_choice_pick === "rock") {
+        alert("you lose");
+    } else if (computuer_choice_pick === "paper") {
+        alert("you win");
+    }
+}
 
 //checks if you are computer won
-if (your_choice_string === "rock" && computuer_choice_pick === "rock") {
-    alert("draw");
-} else if (your_choice_string === "paper" && computuer_choice_pick === "paper") {
-    alert("draw");
-} else if (your_choice_string === "scissors" && computuer_choice_pick === "scissors") {
-    alert("draw");
-}  else if (your_choice_string === "rock" && computuer_choice_pick === "paper") {
-    alert("you lose");
-} else if (your_choice_string === "rock" && computuer_choice_pick === "scissors") {
-    alert("you win");
-} else if (your_choice_string === "paper" && computuer_choice_pick === "rock") {
-    alert("you win");
-} else if (your_choice_string === "paper" && computuer_choice_pick === "scissors") {
-    alert("you lose");
-} else if (your_choice_string === "scissors" && computuer_choice_pick === "rock") {
-    alert("you lose");
-} else if (your_choice_string === "scissors" && computuer_choice_pick === "paper") {
-    alert("you lose");
-}
